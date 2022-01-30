79a03018912165d391aa061e6046194f18eaefdf
Philippe Grégoire <git@pgregoire.xyz>
2022-01-30 14:46:11 -0500

    add license

--- tools/heroku-set-config.sh
+++ tools/heroku-set-config.sh
@@ -7,4 +7,3 @@ heroku config:set -a "${APP_NAME}" "DISCORD_CLIENT_ID=${DISCORD_CLIENT_ID}"
 heroku config:set -a "${APP_NAME}" "DISCORD_CLIENT_SECRET=${DISCORD_CLIENT_SECRET}"
 heroku config:set -a "${APP_NAME}" "DISCORD_PUBLIC_KEY=${DISCORD_PUBLIC_KEY}"
 heroku config:set -a "${APP_NAME}" "DISCORD_GUILD_ID=${DISCORD_GUILD_ID}"
-#heroku config:set -a "${APP_NAME}" 'PORT=33507'
